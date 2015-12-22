fe36eea360969fed932d242fa2c12d8ee12fe220
laharumi <laharumi@users.noreply.github.com>
2015-12-21 23:28:42 -0200

    Update mux8x4.vhd

--- mux8x4.vhd
+++ mux8x4.vhd
@@ -2,22 +2,21 @@
 -- 1 bit de selecao
 -- XARA I
 -- 20151028
+-- 20151217
 
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity mux4x1 is
+entity mux8x4 is
 	port (
-		 I0, I1, I2, I3 	: in std_logic;
-		 Q 					: out std_logic;
-		 sel 				: in std_logic_vector (1 downto 0)
+		 I1, I0 		: in std_logic_vector (3 downto 0);
+		 Q 				: out std_logic_vector(3 downto 0);
+		 sel	 		: in std_logic
 	);
-end mux4x1;
+end mux8x4;
 
-architecture mux of mux4x1 is
+architecture mux of mux8x4 is
 begin
-	Q <= I0 when Sel = "00";
-	Q <= I1 when Sel = "01";
-	Q <= I2 when Sel = "10";
-	Q <= I3 when Sel = "11";
+	Q <= I0 when sel = '0';
+	Q <= I1 when sel = '1';
 end mux;
